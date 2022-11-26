02d74f2965b88fa767c4395ff87742f949f0c276
moonsun <woowsnu@gmail.com>
2022-11-26 20:08:44 +0900

    fix : 사용하지 않은 import 경로 수정
    - 사용하지 않는 경로로 인한 vercel 배포 오류 수정

--- src/pages/cocktails/Cocktail.jsx
+++ src/pages/cocktails/Cocktail.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
-import { share } from "../../utils/share";
 import Likes from "../../UI/Likes";
 import styled from "styled-components";
 import Skeleton from "../../UI/Skeleton";
